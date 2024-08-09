84ec624181a7caa9a8285f415ae75eef8e6636db
Christoph Wurst <christoph@winzerhof-wurst.at>
2024-08-09 07:25:09 +0200

    docs: Release readme
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- templates/main.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * SPDX-FileCopyrightText: 2023 Nextcloud contributors
- * SPDX-License-Identifier: AGPL-3.0-or-later
- */
-?>
-<div id="content_polls" />
--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * SPDX-FileCopyrightText: 2017 Nextcloud contributors
- * SPDX-License-Identifier: AGPL-3.0-or-later
- */
-require_once __DIR__ . '/../../../lib/base.php';
-require_once __DIR__ . '/../vendor/autoload.php';
